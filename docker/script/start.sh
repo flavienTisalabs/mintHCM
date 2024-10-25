afbc163e269df938f2fb35f081e8c64becc384d0
Flavien5151 <flavien.mazier@gmail.com>
2024-10-25 17:56:11 +0200

    start

--- docker/script/start.sh
+++ docker/script/start.sh
@@ -1,48 +1,51 @@
 #!/bin/bash
 
+# Variables
+REPO_URL="https://github.com/flavienTisalabs/mintHCM.git"
+DEST_DIR="/var/www/MintHCM"
+TEMP_DIR=$(mktemp -d)
 FILE=/var/www/html/.initialized
 
-if [ -f "$FILE" ]; then
-  # Run services
-  service cron start
-  service apache2 start
-  echo "RUN SERVICE"
+# Always download the latest code from Git
+if [ -d "$DEST_DIR" ]; then
+    echo "Updating MintHCM from Git repository..."
+    cd $DEST_DIR
+    git pull origin main 
 else
-  # Download MintHCM
-  echo "Download MintHCM"
-  minthcm_temp=$(mktemp -d)
-  #git clone https://github.com/minthcm/minthcm.git $minthcm_temp
-  git clone https://github.com/flavienTisalabs/mintHCM.git $minthcm_temp
-  cp -R $minthcm_temp/* /var/www/MintHCM/
-  rm -r $minthcm_temp
-  #cp -R C:/Users/flavi/Desktop/Tisalabs/minthcm-4.1.0.1/. /var/www/MintHCM/
+    echo "Cloning MintHCM from Git repository..."
+    git clone $REPO_URL $TEMP_DIR
+    cp -R $TEMP_DIR/* $DEST_DIR/
+    rm -r $TEMP_DIR
+fi
 
-  php /var/www/script/generate_config.php
-  chown -R www-data:www-data /var/www/MintHCM
-  chmod -R 755 /var/www/MintHCM
-  chmod -R 755 /var/www/MintHCM/legacy/cache
-  chmod -R 755 /var/www/MintHCM/legacy/upload
- 
-  # Check if the config_si.php file was generated
-  if [[ ! -f /var/www/MintHCM/configMint4 ]]; then
+# Generate config
+php /var/www/script/generate_config.php
+
+# Set permissions
+chown -R www-data:www-data $DEST_DIR
+chmod -R 755 $DEST_DIR
+chmod -R 755 $DEST_DIR/legacy/cache
+chmod -R 755 $DEST_DIR/legacy/upload
+
+# Check if the configMint4 file was generated
+if [[ ! -f $DEST_DIR/configMint4 ]]; then
     printf "Error: Failed to generate configMint4 - please check the configuration\n"
     exit 1
-  fi
+fi
 
-  touch $FILE
-  service apache2 start
+# Start Apache service
+service apache2 start
 
-  # Make the MintHCM installation request
-  printf "Starting MintHCM installation...\n"
-  su -s /bin/bash -c 'php /var/www/MintHCM/MintCLI install < /var/www/MintHCM/configMint4' www-data
+# Make the MintHCM installation request
+printf "Starting MintHCM installation...\n"
+su -s /bin/bash -c 'php /var/www/MintHCM/MintCLI install < /var/www/MintHCM/configMint4' www-data
 
 # Check the exit code
-  if [[ $? -ne 0 ]]; then
+if [[ $? -ne 0 ]]; then
     printf "Error: MintHCM installation failed - please check logs\n"
-  else
+else
     printf "MintHCM installation completed!\n"
-    #add cron and start service
+    # Add cron and start service
     printf "*    *    *    *    *     cd /var/www/MintHCM/legacy; php -f cron.php > /dev/null 2>&1" > /var/spool/cron/crontabs/www-data
     service cron start
-  fi
 fi
